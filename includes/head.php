d1983182d3d36fd201360feb60c377cb491b218c
Clement FAVAREL <clementfavarel.dev@gmail.com>
2023-03-07 19:48:13 +0100

    updated the content and created the head.php in order to include it on each page

---
+++ includes/head.php
@@ -0,0 +1,12 @@
+<head>
+   <meta charset="UTF-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <title>Messaging | <?php $currentPage ?></title>
+   <meta name="description" content="A simple chat app" />
+
+   <!-- FAVICON -->
+
+   <!-- CSS -->
+
+</head>
--- view/login.php
+++ view/login.php
@@ -1,1 +1,9 @@
-login
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once 'includes/head.php'; ?>
+
+<body>
+
+</body>
+
+</html>
