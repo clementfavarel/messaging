e149084e40bd1824e09f1a97dd5a266240206499
Clement FAVAREL <clementfavarel.dev@gmail.com>
2023-03-07 19:14:38 +0100

    created controller.php

---
+++ controllers/php/controller.php
@@ -0,0 +1,31 @@
+<?php
+include_once './models/Model.php';
+
+class Controller
+{
+   public $model;
+   private $conn;
+
+   public function __construct()
+   {
+      $this->model = new Model();
+   }
+
+   public function invoke()
+   {
+      //routes digihub app
+      $page = isset(array_keys($_GET)[0]) ? array_keys($_GET)[0] : 'home';
+
+      $conn = $this->model->getConn();
+
+      switch ($page) {
+         case 'home':
+            $currentPage = 'Accueil';
+            include 'views/home.php';
+            break;
+         default:
+            include 'views/home.php';
+            break;
+      }
+   }
+}
