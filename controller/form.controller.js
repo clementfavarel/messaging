47a8da234bc2cbb96cedd06fcf6782850de9f680
Clement FAVAREL <clementfavarel.dev@gmail.com>
2023-03-07 21:45:08 +0100

    updated controller.js with checkPassword and Email functions

--- controller/form.controller.js
+++ controller/form.controller.js
@@ -21,3 +21,25 @@ function hidePassword() {
       eye_slash.style.display = 'none';
    }
 }
+
+function checkPassword() {
+   const password = document.getElementById('password');
+
+   // if password don't match pattern
+   if (!password.value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)) {
+      password.classList.add('error');
+   } else {
+      password.classList.remove('error');
+   }
+}
+
+function checkEmail() {
+   const email = document.getElementById('email');
+
+   // if email don't match the pattern
+   if (!email.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+      email.classList.add('error');
+   } else {
+      email.classList.remove('error');
+   }
+}
--- view/login.php
+++ view/login.php
@@ -7,10 +7,10 @@
       <form method="post" class="form" id="form">
          <h1 class="center">Connexion</h1>
          <label class="form__label" for="email">Email</label>
-         <input class="form__input" type="email" name="email" id="email" placeholder="mail@exemple.com">
+         <input class="form__input" type="email" name="email" id="email" placeholder="mail@exemple.com" onkeyup="checkEmail();">
          <label class="form__label" for="password">Mot de passe</label>
          <div class="password">
-            <input class="form__input" type="password" name="password" id="password" placeholder="********">
+            <input class="form__input" type="password" name="password" id="password" placeholder="********" onkeyup="checkPassword();">
             <i class="fa-solid fa-eye" onclick="showPassword();" id="eye"></i>
             <i class="fa-solid fa-eye-slash" style="display:none" onclick="hidePassword();" id="eye-slash"></i>
          </div>
