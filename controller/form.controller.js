68b806d64867a389ecfb75d1ac0b41f3eb5c41f5
Clement FAVAREL <clementfavarel.dev@gmail.com>
2023-03-07 21:48:50 +0100

    updated the style of the disabled class when the controller.js works

--- controller/form.controller.js
+++ controller/form.controller.js
@@ -24,22 +24,28 @@ function hidePassword() {
 
 function checkPassword() {
    const password = document.getElementById('password');
+   const submit = document.getElementById('submit');
 
    // if password don't match pattern
    if (!password.value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)) {
       password.classList.add('error');
+      submit.classList.add('disabled');
    } else {
       password.classList.remove('error');
+      submit.classList.remove('disabled');
    }
 }
 
 function checkEmail() {
    const email = document.getElementById('email');
+   const submit = document.getElementById('submit');
 
    // if email don't match the pattern
    if (!email.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
       email.classList.add('error');
+      submit.classList.add('disabled');
    } else {
       email.classList.remove('error');
+      submit.classList.remove('disabled');
    }
 }
