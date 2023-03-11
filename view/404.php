c987ba30847d840bea81b7f075bd90d5eb21fa49
Clement FAVAREL <clementfavarel.dev@gmail.com>
2023-03-11 18:39:34 +0100

    updated 404 html and styling

--- view/404.php
+++ view/404.php
@@ -7,10 +7,12 @@
 </head>
 
 <body>
-   <h1 class="error_title">Oops!</h1>
-   <h2>404 Not Found</h2>
-   <p>Désolé, une erreur s'est produite, la page demandée n'a pas été trouvée !</p>
-   <a href="?home">Retour à l'accueil </a>
+   <div class="container">
+      <h1 class="error_title">Oops!</h1>
+      <h2>404 Not Found</h2>
+      <p>Désolé, une erreur s'est produite, la page demandée n'a pas été trouvée !</p>
+      <a class="btn" href="?home">Retour à l'accueil </a>
+   </div>
 </body>
 
 </html>
