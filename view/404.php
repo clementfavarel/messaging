79a3d5b654c2d5e1bdcd3f1aafffb188da451d35
Clement FAVAREL <clementfavarel.dev@gmail.com>
2023-03-11 17:56:49 +0100

    created the 404 page for error in the php routes

---
+++ view/404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include 'view/include/head.php'; ?>
+
+<head>
+   <link rel="stylesheet" href="styles/404.css">
+</head>
+
+<body>
+   <h1 class="error_title">Oops!</h1>
+   <h2>404 Not Found</h2>
+   <p>Désolé, une erreur s'est produite, la page demandée n'a pas été trouvée !</p>
+   <a href="?home">Retour à l'accueil </a>
+</body>
+
+</html>
